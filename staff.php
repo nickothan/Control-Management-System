201bc103728065572167235141902f8cea0b94d6
Sergio Ramirez <s1nicolasrg@gmail.com>
2019-04-26 20:18:43 -0500

    arquitectura del Staf.

---
+++ staff.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Basico</title>
+</head>
+<body>
+    <div id="header">
+        <h1>S. Corp</h1>
+    </div>
+    <div id="principal">
+        <table id="estructura">
+            <tr>
+                <td id="navegacion">
+                    aadsf;
+                </td>
+                <td id="pagina">
+                    <h2>Menu de Staff</h2>
+                    <p>Bienbenido al area de staff.</p>
+                    <ul>
+                        <li><a href="contenido.php">Administrar contenido de la pagina</a></li>
+                        <li><a href="nuevo_usuario.php">Agregar usuario staff</a></li>
+                        <li><a href="logout.php">log out</a></li>
+                </ul>
+                </td>
+            </tr>
+        </table>
+    </div>
+    <div id="footer">Derechos recervados 2011, S. Corp</div>
+</body>
+</html>
